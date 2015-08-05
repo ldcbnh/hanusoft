a4ae7885d19cd2aec2ddc22d1a99b0ca23b45292
Bnh DC <ldcbnh@gmail.com>
2015-08-05 22:29:19 +0700

    Indent admin.php file

--- admin.php
+++ admin.php
@@ -5,17 +5,17 @@ if (isset($SESSION['admin']) && $SESSION['admin'] == true) {
 
 } else {
 	echo '<center>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<div class="panel panel-info">
-					  <div class="panel-heading">
-							<h3 class="panel-title">You dont have permission to view this page</h3>
-					  </div>
-					  <div class="panel-body">
-							<a href="index.php" ><button type="button" class="btn btn-success">Return to homepage</button></a>
-					  </div>
-					</div>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="panel panel-info">
+				  <div class="panel-heading">
+						<h3 class="panel-title">You dont have permission to view this page</h3>
+				  </div>
+				  <div class="panel-body">
+						<a href="index.php" ><button type="button" class="btn btn-success">Return to homepage</button></a>
+				  </div>
 				</div>
-			</center>
+			</div>
+		</center>
 		';
 }
 
